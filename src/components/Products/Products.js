3bfc7b29faefa44a233ecf24954bb3c7536b78c4
noubarijou <85699669+noubarijou@users.noreply.github.com>
2021-12-05 14:41:03 -0300

    up to the cart layout

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,12 +3,9 @@ import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running Shoes', price: '$5', image: 'https://cdn.sweatband.com/mizuno_wave_inspire_14_ladies_running_shoes_aw18_mizuno_wave_inspire_14_ladies_running_shoes_aw18-_angled_2000x2000.jpg'},
-    {id: 2, name: 'macbook', description: 'Apple macbook', price: '$10', image: 'https://images.techhive.com/images/article/2014/12/macbook_air-100538062-large.jpg'}
-];
 
-const Products = () => {
+
+const Products = ({products, onAddToCart}) => {
     const classes = useStyles();
     return (
         <main className={classes.content}>
@@ -16,7 +13,7 @@ const Products = () => {
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product} />
+                    <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
